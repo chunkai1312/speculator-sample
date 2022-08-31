a81e9f44d045ceb946dea7ee8d44e636f51abc79
Chun-Kai Wang <chunkai1312@gmail.com>
2022-08-31 12:05:45 +0800

    feat(scraper): add scraper module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
+import { ScraperModule } from './scraper/scraper.module';
 
 @Module({
-  imports: [],
+  imports: [ScraperModule],
 })
 export class AppModule {}
