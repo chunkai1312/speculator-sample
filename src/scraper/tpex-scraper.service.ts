29ee0f05a3a6cba6e9e387e95d367a816c77391e
Chun-Kai Wang <chunkai1312@gmail.com>
2022-09-01 00:42:48 +0800

    feat(scraper): fetch TPEx market breadth

--- src/scraper/tpex-scraper.service.ts
+++ src/scraper/tpex-scraper.service.ts
@@ -42,4 +42,41 @@ export class TpexScraperService {
 
     return data;
   }
+
+  async fetchMarketBreadth(date: string) {
+    const dt = DateTime.fromISO(date);
+    const formattedDate = `${dt.get('year') - 1911}/${dt.toFormat('MM/dd')}`;  //將 ISO Date 轉換成 `民國年/MM/dd` 格式
+    const query = new URLSearchParams({   // 建立 URL 查詢參數
+      l: 'zh-tw',                         // 指定語系為正體中文
+      d: formattedDate,                   // 指定日期
+      o: 'json',                          // 指定回應格式為 JSON
+    });
+    const url = `https://www.tpex.org.tw/web/stock/aftertrading/market_highlight/highlight_result.php?${query}`;
+
+    // 取得回應資料
+    const responseData = await firstValueFrom(this.httpService.get(url))
+      .then(response => (response.data.iTotalRecords > 0) && response.data);
+
+    // 若該日期非交易日或尚無成交資訊則回傳 null
+    if (!responseData) return null;
+
+    const { upNum, upStopNum, downNum, downStopNum, noChangeNum, noTradeNum } = responseData;
+
+    // 整理回應資料
+    const [ up, limitUp, down, limitDown, unchanged, unmatched ] = [
+      upNum, upStopNum, downNum, downStopNum, noChangeNum, noTradeNum
+    ].map(value => numeral(value).value());   // 轉為數字格式
+
+    const data = {
+      date,
+      up,
+      limitUp,
+      down,
+      limitDown,
+      unchanged,
+      unmatched,
+    };
+
+    return data;
+  }
 }
