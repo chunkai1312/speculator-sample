199ea06acb30b0f65506882f60647128f060a157
Chun-Kai Wang <chunkai1312@gmail.com>
2022-09-01 20:08:34 +0800

    feat(scraper): fetch TPEx institutional investors trades

--- src/scraper/tpex-scraper.service.ts
+++ src/scraper/tpex-scraper.service.ts
@@ -79,4 +79,77 @@ export class TpexScraperService {
 
     return data;
   }
+
+  async fetchInstInvestorsTrades(date: string) {
+    const dt = DateTime.fromISO(date);
+    const formattedDate = `${dt.get('year') - 1911}/${dt.toFormat('MM')}/${dt.toFormat('dd')}`; // 將 ISO Date 格式轉換成 `民國年/MM/dd`
+    const query = new URLSearchParams({   // 建立 URL 查詢參數
+      l: 'zh-tw',                         // 指定語系為正體中文
+      t: 'D',                             // 輸出日報表
+      d: formattedDate,                   // 指定日期
+      o: 'json',                          // 指定回應格式為 JSON
+    });
+    const url = `https://www.tpex.org.tw/web/stock/3insti/3insti_summary/3itrdsum_result.php?${query}`;
+
+    // 取得回應資料
+    const responseData = await firstValueFrom(this.httpService.get(url))
+      .then(response => (response.data.iTotalRecords > 0) && response.data);
+
+    // 若該日期非交易日或尚無成交資訊則回傳 null
+    if (!responseData) return null;
+
+    // 整理回應資料
+    const raw = responseData.aaData
+      .map(data => data.slice(1)).flat()              // 取買賣金額並減少一層陣列嵌套
+      .map(data => numeral(data).value() || +data);   // 轉為數字格式
+
+    const [
+      foreignInvestorsBuy,                // 外資及陸資合計買進金額
+      foreignInvestorsSell,               // 外資及陸資合計賣出金額
+      foreignInvestorsNetBuySell,         // 外資及陸資合計買賣超
+      foreignDealersExcludedBuy,          // 外資及陸資(不含自營商)買進金額
+      foreignDealersExcludedSell,         // 外資及陸資(不含自營商)賣出金額
+      foreignDealersExcludedNetBuySell,   // 外資及陸資(不含自營商)買賣超
+      foreignDealersBuy,                  // 外資自營商買進金額
+      foreignDealersSell,                 // 外資自營商賣出金額
+      foreignDealersNetBuySell,           // 外資自營商買賣超
+      sitcBuy,                            // 投信買進金額
+      sitcSell,                           // 投信賣出金額
+      sitcNetBuySell,                     // 投信買賣超
+      dealersBuy,                         // 自營商合計買進金額
+      dealersSell,                        // 自營商合計賣出金額
+      dealersNetBuySell,                  // 自營商合計買賣超
+      dealersProprietaryBuy,              // 自營商(自行買賣)買進金額
+      dealersProprietarySell,             // 自營商(自行買賣)賣出金額
+      dealersProprietaryNetBuySell,       // 自營商(自行買賣)買賣超
+      dealersHedgeBuy,                    // 自營商(避險)買進金額
+      dealersHedgeSell,                   // 自營商(避險)賣出金額
+      dealersHedgeNetBuySell,             // 自營商(避險)買賣超
+    ] = raw;
+
+    return {
+      date,
+      dealersProprietaryBuy,
+      dealersProprietarySell,
+      dealersProprietaryNetBuySell,
+      dealersHedgeBuy,
+      dealersHedgeSell,
+      dealersHedgeNetBuySell,
+      dealersBuy,
+      dealersSell,
+      dealersNetBuySell,
+      sitcBuy,
+      sitcSell,
+      sitcNetBuySell,
+      foreignDealersExcludedBuy,
+      foreignDealersExcludedSell,
+      foreignDealersExcludedNetBuySell,
+      foreignDealersBuy,
+      foreignDealersSell,
+      foreignDealersNetBuySell,
+      foreignInvestorsBuy,
+      foreignInvestorsSell,
+      foreignInvestorsNetBuySell,
+    };
+  }
 }
