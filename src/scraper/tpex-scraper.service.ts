fcbcfb4e989980504ebb47d414e161ca91276801
Chun-Kai Wang <chunkai1312@gmail.com>
2022-09-03 11:45:21 +0800

    feat(scraper): fetch TPEx margin transactions

--- src/scraper/tpex-scraper.service.ts
+++ src/scraper/tpex-scraper.service.ts
@@ -9,12 +9,16 @@ export class TpexScraperService {
   constructor(private httpService: HttpService) {}
 
   async fetchMarketTrades(date: string) {
+    // 將 `date` 轉換成 `民國年/MM` 格式
     const dt = DateTime.fromISO(date);
-    const formattedDate = `${dt.get('year') - 1911}/${dt.toFormat('MM')}`;  //將 ISO Date 轉換成 `民國年/MM` 格式
-    const query = new URLSearchParams({   // 建立 URL 查詢參數
-      l: 'zh-tw',                         // 指定語系為正體中文
-      d: formattedDate,                   // 指定日期
-      o: 'json',                          // 指定回應格式為 JSON
+    const year = dt.get('year') - 1911;
+    const formattedDate = `${year}/${dt.toFormat('MM')}`;
+
+    // 建立 URL 查詢參數
+    const query = new URLSearchParams({
+      l: 'zh-tw',         // 指定語系為正體中文
+      d: formattedDate,   // 指定日期
+      o: 'json',          // 指定回應格式為 JSON
     });
     const url = `https://www.tpex.org.tw/web/stock/aftertrading/daily_trading_index/st41_result.php?${query}`;
 
@@ -27,16 +31,26 @@ export class TpexScraperService {
 
     // 整理回應資料
     const data = responseData.aaData.map(row => {
-      const [ date, ...values ] = row;  // [ 日期, 成交股數, 金額, 筆數, 櫃買指數, 漲/跌 ]
+      // [ 日期, 成交股數, 金額, 筆數, 櫃買指數, 漲/跌 ]
+      const [ date, ...values ] = row;
 
       // 將 `民國年/MM/dd` 的日期格式轉換成 `yyyy-MM-dd`
       const [ year, month, day ] = date.split('/');
-      const formattedDate = DateTime.fromFormat(`${+year + 1911}${month}${day}`, 'yyyyMMdd').toISODate();
+      const formatted = `${+year + 1911}${month}${day}`;
+      const formattedDate = DateTime.fromFormat(formatted, 'yyyyMMdd').toISODate();
 
       // 轉為數字格式
-      const [ tradeVolume, tradeValue, transaction, price, change ] = values.map(value => numeral(value).value());
+      const [ tradeVolume, tradeValue, transaction, price, change ]
+        = values.map(value => numeral(value).value());
 
-      return { date: formattedDate, tradeVolume, tradeValue, transaction, price, change };
+      return {
+        date: formattedDate,
+        tradeVolume,
+        tradeValue,
+        transaction,
+        price,
+        change,
+      };
     })
     .find(data => data.date === date) || null;  // 取得目標日期的成交資訊
 
@@ -44,12 +58,16 @@ export class TpexScraperService {
   }
 
   async fetchMarketBreadth(date: string) {
+    // `date` 轉換成 `民國年/MM/dd` 格式
     const dt = DateTime.fromISO(date);
-    const formattedDate = `${dt.get('year') - 1911}/${dt.toFormat('MM/dd')}`;  //將 ISO Date 轉換成 `民國年/MM/dd` 格式
-    const query = new URLSearchParams({   // 建立 URL 查詢參數
-      l: 'zh-tw',                         // 指定語系為正體中文
-      d: formattedDate,                   // 指定日期
-      o: 'json',                          // 指定回應格式為 JSON
+    const year = dt.get('year') - 1911;
+    const formattedDate = `${year}/${dt.toFormat('MM/dd')}`;
+
+    // 建立 URL 查詢參數
+    const query = new URLSearchParams({
+      l: 'zh-tw',         // 指定語系為正體中文
+      d: formattedDate,   // 指定日期
+      o: 'json',          // 指定回應格式為 JSON
     });
     const url = `https://www.tpex.org.tw/web/stock/aftertrading/market_highlight/highlight_result.php?${query}`;
 
@@ -60,9 +78,8 @@ export class TpexScraperService {
     // 若該日期非交易日或尚無成交資訊則回傳 null
     if (!responseData) return null;
 
-    const { upNum, upStopNum, downNum, downStopNum, noChangeNum, noTradeNum } = responseData;
-
     // 整理回應資料
+    const { upNum, upStopNum, downNum, downStopNum, noChangeNum, noTradeNum } = responseData;
     const [ up, limitUp, down, limitDown, unchanged, unmatched ] = [
       upNum, upStopNum, downNum, downStopNum, noChangeNum, noTradeNum
     ].map(value => numeral(value).value());   // 轉為數字格式
@@ -81,13 +98,17 @@ export class TpexScraperService {
   }
 
   async fetchInstInvestorsTrades(date: string) {
+    // `date` 轉換成 `民國年/MM/dd` 格式
     const dt = DateTime.fromISO(date);
-    const formattedDate = `${dt.get('year') - 1911}/${dt.toFormat('MM')}/${dt.toFormat('dd')}`; // 將 ISO Date 格式轉換成 `民國年/MM/dd`
-    const query = new URLSearchParams({   // 建立 URL 查詢參數
-      l: 'zh-tw',                         // 指定語系為正體中文
-      t: 'D',                             // 輸出日報表
-      d: formattedDate,                   // 指定日期
-      o: 'json',                          // 指定回應格式為 JSON
+    const year = dt.get('year') - 1911;
+    const formattedDate = `${year}/${dt.toFormat('MM/dd')}`;
+
+    // 建立 URL 查詢參數
+    const query = new URLSearchParams({
+      l: 'zh-tw',         // 指定語系為正體中文
+      t: 'D',             // 指定輸出日報表
+      d: formattedDate,   // 指定資料日期
+      o: 'json',          // 指定回應格式為 JSON
     });
     const url = `https://www.tpex.org.tw/web/stock/3insti/3insti_summary/3itrdsum_result.php?${query}`;
 
@@ -100,16 +121,16 @@ export class TpexScraperService {
 
     // 整理回應資料
     const raw = responseData.aaData
-      .map(data => data.slice(1)).flat()              // 取買賣金額並減少一層陣列嵌套
+      .map(data => data.slice(1)).flat()  // 取買賣金額並減少一層陣列嵌套
       .map(data => numeral(data).value() || +data);   // 轉為數字格式
 
     const [
       foreignInvestorsBuy,                // 外資及陸資合計買進金額
       foreignInvestorsSell,               // 外資及陸資合計賣出金額
       foreignInvestorsNetBuySell,         // 外資及陸資合計買賣超
-      foreignDealersExcludedBuy,          // 外資及陸資(不含自營商)買進金額
-      foreignDealersExcludedSell,         // 外資及陸資(不含自營商)賣出金額
-      foreignDealersExcludedNetBuySell,   // 外資及陸資(不含自營商)買賣超
+      foreignDealersExcludedBuy,          // 外資及陸資(不含外資自營商)買進金額
+      foreignDealersExcludedSell,         // 外資及陸資(不含外資自營商)賣出金額
+      foreignDealersExcludedNetBuySell,   // 外資及陸資(不含外資自營商)買賣超
       foreignDealersBuy,                  // 外資自營商買進金額
       foreignDealersSell,                 // 外資自營商賣出金額
       foreignDealersNetBuySell,           // 外資自營商買賣超
@@ -129,18 +150,6 @@ export class TpexScraperService {
 
     return {
       date,
-      dealersProprietaryBuy,
-      dealersProprietarySell,
-      dealersProprietaryNetBuySell,
-      dealersHedgeBuy,
-      dealersHedgeSell,
-      dealersHedgeNetBuySell,
-      dealersBuy,
-      dealersSell,
-      dealersNetBuySell,
-      sitcBuy,
-      sitcSell,
-      sitcNetBuySell,
       foreignDealersExcludedBuy,
       foreignDealersExcludedSell,
       foreignDealersExcludedNetBuySell,
@@ -150,6 +159,85 @@ export class TpexScraperService {
       foreignInvestorsBuy,
       foreignInvestorsSell,
       foreignInvestorsNetBuySell,
+      sitcBuy,
+      sitcSell,
+      sitcNetBuySell,
+      dealersProprietaryBuy,
+      dealersProprietarySell,
+      dealersProprietaryNetBuySell,
+      dealersHedgeBuy,
+      dealersHedgeSell,
+      dealersHedgeNetBuySell,
+      dealersBuy,
+      dealersSell,
+      dealersNetBuySell,
+    };
+  }
+
+  async fetchMarginTransactions(date: string) {
+    // `date` 轉換成 `民國年/MM/dd` 格式
+    const dt = DateTime.fromISO(date);
+    const year = dt.get('year') - 1911;
+    const formattedDate = `${year}/${dt.toFormat('MM/dd')}`;
+
+    // 建立 URL 查詢參數
+    const query = new URLSearchParams({
+      l: 'zh-tw',       // 指定語系為正體中文
+      d: formattedDate, // 指定日期
+      o: 'json',        // 指定回應格式為 JSON
+    });
+    const url = `https://www.tpex.org.tw/web/stock/margin_trading/margin_balance/margin_bal_result.php?${query}`;
+
+    // 取得回應資料
+    const responseData = await firstValueFrom(this.httpService.get(url))
+      .then(response => (response.data.iTotalRecords > 0) && response.data);
+
+    // 若該日期非交易日或尚無成交資訊則回傳 null
+    if (!responseData) return null;
+
+    // 整理回應資料
+    const raw = [
+      ...responseData.tfootData_one,
+      ...responseData.tfootData_two
+    ]  // 取出融資融券統計
+      .map(data => numeral(data).value()) // 轉為數字格式
+      .filter(data => data);  // 移除 null 值
+
+    const [
+      marginBalancePrev,        // 融資(交易單位)-前日餘額
+      marginPurchase,           // 融資(交易單位)-買進
+      marginSale,               // 融資(交易單位)-賣出
+      cashRedemption,           // 融資(交易單位)-現金(券)償還
+      marginBalance,            // 融資(交易單位)-今日餘額
+      shortBalancePrev,         // 融券(交易單位)-前日餘額
+      shortCovering,            // 融券(交易單位)-買進
+      shortSale,                // 融券(交易單位)-賣出
+      stockRedemption,          // 融券(交易單位)-現金(券)償還
+      shortBalance,             // 融券(交易單位)-今日餘額
+      marginBalanceValuePrev,   // 融資金額(仟元)-前日餘額
+      marginPurchaseValue,      // 融資金額(仟元)-買進
+      marginSaleValue,          // 融資金額(仟元)-賣出
+      cashRedemptionValue,      // 融資金額(仟元)-現金(券)償還
+      marginBalanceValue,       // 融資金額(仟元)-今日餘額
+    ] = raw;
+
+    // 計算融資餘額增減(交易單位)
+    const marginChange = marginBalance - marginBalancePrev;
+
+    // 計算融資餘額增減(仟元)
+    const marginChangeValue = marginBalanceValue - marginBalanceValuePrev;
+
+    // 計算融券餘額增減(交易單位)
+    const shortChange = shortBalance - shortBalancePrev;
+
+    return {
+      date,
+      marginBalance,
+      marginChange,
+      marginBalanceValue,
+      marginChangeValue,
+      shortBalance,
+      shortChange,
     };
   }
 }
