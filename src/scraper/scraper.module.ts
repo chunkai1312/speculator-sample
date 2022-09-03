6ac574fc13bdf1d5be336c9b5c9a3e891710dcac
Chun-Kai Wang <chunkai1312@gmail.com>
2022-09-04 02:19:29 +0800

    feat(scraper): add taifex scraper service

--- src/scraper/scraper.module.ts
+++ src/scraper/scraper.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { TwseScraperService } from './twse-scraper.service';
 import { TpexScraperService } from './tpex-scraper.service';
+import { TaifexScraperService } from './taifex-scraper.service';
 
 @Module({
   imports: [HttpModule],
-  providers: [TwseScraperService, TpexScraperService],
+  providers: [TwseScraperService, TpexScraperService, TaifexScraperService],
 })
 export class ScraperModule {}
