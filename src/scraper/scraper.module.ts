3d95ce8ddb6dae0cb01deab5a5abc5366efd9315
Chun-Kai Wang <chunkai1312@gmail.com>
2022-09-09 21:56:26 +0800

    feat(scraper): add yahoo finance service

--- src/scraper/scraper.module.ts
+++ src/scraper/scraper.module.ts
@@ -3,9 +3,10 @@ import { HttpModule } from '@nestjs/axios';
 import { TwseScraperService } from './twse-scraper.service';
 import { TpexScraperService } from './tpex-scraper.service';
 import { TaifexScraperService } from './taifex-scraper.service';
+import { YahooFinanceService } from './yahoo-finance.service';
 
 @Module({
   imports: [HttpModule],
-  providers: [TwseScraperService, TpexScraperService, TaifexScraperService],
+  providers: [TwseScraperService, TpexScraperService, TaifexScraperService, YahooFinanceService],
 })
 export class ScraperModule {}
