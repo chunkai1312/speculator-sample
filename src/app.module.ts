a6054ca6d93ce629b2dec639c4599d8358d1c250
Chun-Kai Wang <chunkai1312@gmail.com>
2022-09-19 01:13:55 +0800

    feat(scraper): add mongoose connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ScraperModule } from './scraper/scraper.module';
 
 @Module({
-  imports: [ScraperModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    ScraperModule,
+  ],
 })
 export class AppModule {}
