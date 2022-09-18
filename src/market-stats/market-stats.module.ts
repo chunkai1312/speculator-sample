09b77122b4660f3f484d86f5bdc954bbc3957110
Chun-Kai Wang <chunkai1312@gmail.com>
2022-09-19 02:05:16 +0800

    feat(scraper): add market stats repository

--- src/market-stats/market-stats.module.ts
+++ src/market-stats/market-stats.module.ts
@@ -1,4 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MarketStats, MarketStatsSchema } from './market-stats.schema';
+import { MarketStatsRepository } from './market-stats.repository';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: MarketStats.name, schema: MarketStatsSchema },
+    ]),
+  ],
+  providers: [MarketStatsRepository],
+  exports: [MarketStatsRepository],
+})
 export class MarketStatsModule {}
