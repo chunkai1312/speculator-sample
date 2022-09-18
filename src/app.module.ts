3d8fa8602c35abb2119d4b2508031feb73727b47
Chun-Kai Wang <chunkai1312@gmail.com>
2022-09-19 01:30:47 +0800

    feat(scraper): add market stats module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ScraperModule } from './scraper/scraper.module';
+import { MarketStatsModule } from './market-stats/market-stats.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGODB_URI),
     ScraperModule,
+    MarketStatsModule,
   ],
 })
 export class AppModule {}
