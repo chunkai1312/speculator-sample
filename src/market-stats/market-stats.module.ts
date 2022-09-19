fecbcaa1e824e89df3fca7abb94694963383d139
Chun-Kai Wang <chunkai1312@gmail.com>
2022-09-19 10:05:41 +0800

    feat(scraper): add market stats service

--- src/market-stats/market-stats.module.ts
+++ src/market-stats/market-stats.module.ts
@@ -2,14 +2,17 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MarketStats, MarketStatsSchema } from './market-stats.schema';
 import { MarketStatsRepository } from './market-stats.repository';
+import { MarketStatsService } from './market-stats.service';
+import { ScraperModule } from '../scraper/scraper.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: MarketStats.name, schema: MarketStatsSchema },
     ]),
+    ScraperModule,
   ],
-  providers: [MarketStatsRepository],
+  providers: [MarketStatsRepository, MarketStatsService],
   exports: [MarketStatsRepository],
 })
 export class MarketStatsModule {}
