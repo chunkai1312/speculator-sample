bd446e16067d6b983e150623d560d68009f735fd
Chun-Kai Wang <chunkai1312@gmail.com>
2022-09-19 12:14:25 +0800

    feat(scraper): add ticker module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { ScheduleModule } from '@nestjs/schedule';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ScraperModule } from './scraper/scraper.module';
 import { MarketStatsModule } from './market-stats/market-stats.module';
+import { TickerModule } from './ticker/ticker.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { MarketStatsModule } from './market-stats/market-stats.module';
     MongooseModule.forRoot(process.env.MONGODB_URI),
     ScraperModule,
     MarketStatsModule,
+    TickerModule,
   ],
 })
 export class AppModule {}
