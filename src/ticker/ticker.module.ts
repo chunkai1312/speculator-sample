f472590b1d4e28db2a4cf71c2c5c6f2618f3cbfd
Chun-Kai Wang <chunkai1312@gmail.com>
2022-09-19 12:17:10 +0800

    feat(scraper): add ticker service

--- src/ticker/ticker.module.ts
+++ src/ticker/ticker.module.ts
@@ -1,4 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Ticker, TickerSchema } from './ticker.schema';
+import { TickerRepository } from './ticker.repository';
+import { TickerService } from './ticker.service';
+import { ScraperModule } from '../scraper/scraper.module';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Ticker.name, schema: TickerSchema },
+    ]),
+    ScraperModule,
+  ],
+  providers: [TickerRepository, TickerService],
+  exports: [TickerRepository, TickerService],
+})
 export class TickerModule {}
