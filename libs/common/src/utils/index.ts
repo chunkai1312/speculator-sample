e0707983e0dea87d4646836c7912e20fca669fd5
Chun-Kai Wang <chunkai1312@gmail.com>
2022-09-20 12:20:59 +0800

    feat(libs): add utility function to get market name

--- libs/common/src/utils/index.ts
+++ libs/common/src/utils/index.ts
@@ -1,3 +1,4 @@
 export * from './get-twse-index-symbol-by-name.util';
 export * from './get-tpex-index-symbol-by-name.util';
 export * from './is-warrant.util';
+export * from './get-market-name.util';
