8b90a9c83703b4d9f8ef200954a34a44ce1f1293
Chun-Kai Wang <chunkai1312@gmail.com>
2022-09-20 12:18:20 +0800

    feat(scraper): get money flow

--- src/ticker/ticker.repository.ts
+++ src/ticker/ticker.repository.ts
@@ -1,6 +1,9 @@
+import { DateTime } from 'luxon';
+import { find } from 'lodash';
+import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { TickerType, Market, Index } from '@speculator/common';
 import { Ticker, TickerDocument } from './ticker.schema';
 
 @Injectable()
@@ -12,5 +15,36 @@ export class TickerRepository {
   async updateTicker(ticker: Partial<Ticker>) {
     const { date, symbol } = ticker;
     return this.model.updateOne({ date, symbol }, ticker, { upsert: true });
+  }
+
+  async getMoneyFlow(options?: { date?: string, market?: Market }) {
+    const date = options?.date || DateTime.local().toISODate();
+    const market = options?.market || Market.TSE;
+
+    const results = await this.model.aggregate([
+      { $match: {
+          date: { $lte: date },
+          type: TickerType.Index,
+          market: market || { $ne: null },
+          symbol: { $nin: [Index.NonElectronics, Index.NonFinance, Index.NonFinanceNonElectronics] },
+        },
+      },
+      { $project: { _id: 0, __v: 0, createdAt: 0 , updatedAt: 0 } },
+      { $group: { _id: '$date', data: { $push: '$$ROOT' } } },
+      { $sort: { _id: -1 } },
+      { $limit: 2 },
+    ]);
+
+    const [ tickers, tickersPrev ] = results.map(doc => doc.data);
+
+    const data = tickers.map(doc => {
+      const tradeValuePrev = find(tickersPrev, { symbol: doc.symbol }).tradeValue;
+      const tradeWeightPrev = find(tickersPrev, { symbol: doc.symbol }).tradeWeight;
+      const tradeValueChange = parseFloat((doc.tradeValue - tradeValuePrev).toPrecision(12));
+      const tradeWeightChange = parseFloat((doc.tradeWeight - tradeWeightPrev).toPrecision(12));
+      return { ...doc, tradeValuePrev, tradeWeightPrev, tradeValueChange, tradeWeightChange };
+    });
+
+    return data;
   }
 }
