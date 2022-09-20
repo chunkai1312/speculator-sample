1eda2041dfd2fd3992f5d39dea74fae5c8c58ab6
Chun-Kai Wang <chunkai1312@gmail.com>
2022-09-20 12:22:10 +0800

    feat(libs): add utility function to get sector name

--- libs/common/src/utils/index.ts
+++ libs/common/src/utils/index.ts
@@ -2,3 +2,4 @@ export * from './get-twse-index-symbol-by-name.util';
 export * from './get-tpex-index-symbol-by-name.util';
 export * from './is-warrant.util';
 export * from './get-market-name.util';
+export * from './get-sector-name.util';
