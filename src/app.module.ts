c96bd7db7ce524c49b33139de961977378bafad7
Chun-Kai Wang <chunkai1312@gmail.com>
2022-09-21 23:50:50 +0800

    feat(scraper): add mailer module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { Module, OnApplicationBootstrap, Logger } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
 import { MongooseModule } from '@nestjs/mongoose';
+import { MailerModule } from '@nestjs-modules/mailer';
 import { ScraperModule } from './scraper/scraper.module';
 import { MarketStatsModule } from './market-stats/market-stats.module';
 import { TickerModule } from './ticker/ticker.module';
@@ -15,6 +16,25 @@ import { ReportModule } from './report/report.module';
     ConfigModule.forRoot(),
     ScheduleModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGODB_URI),
+    MailerModule.forRoot({
+      transport: {
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+          type: 'oauth2',
+          user: process.env.GOOGLE_USER,
+          clientId: process.env.GOOGLE_CLIENT_ID,
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+          accessToken: process.env.GOOGLE_ACCESS_TOKEN,
+          refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+        },
+      },
+      defaults: {
+        from: process.env.MAILER_FROM,
+        to: process.env.MAILER_TO,
+      },
+    }),
     ScraperModule,
     MarketStatsModule,
     TickerModule,
