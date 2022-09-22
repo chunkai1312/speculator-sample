86315750e8b33b018553451d4feb65b6fea2cf94
Chun-Kai Wang <chunkai1312@gmail.com>
2022-09-22 18:27:53 +0800

    chore(monitor): setup app module

--- apps/monitor/src/app.module.ts
+++ apps/monitor/src/app.module.ts
@@ -1,4 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { RedisModule } from '@liaoliaots/nestjs-redis';
+import { LineNotifyModule } from 'nest-line-notify';
 
-@Module({})
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    RedisModule.forRoot({
+      config: { url: process.env.REDIS_URL },
+    }),
+    LineNotifyModule.forRoot({
+      accessToken: process.env.LINE_NOTIFY_ACCESS_TOKEN,
+    }),
+  ],
+})
 export class AppModule {}
