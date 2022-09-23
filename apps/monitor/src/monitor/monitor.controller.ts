b850ecbeed62ba9952169067932e2097d5044b58
Chun-Kai Wang <chunkai1312@gmail.com>
2022-09-23 18:40:10 +0800

    feat(monitor): add monitor api

--- apps/monitor/src/monitor/monitor.controller.ts
+++ apps/monitor/src/monitor/monitor.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Body, Param, HttpCode } from '@nestjs/common';
+import { MonitorService } from './monitor.service';
+import { CreateAlertDto } from './dto/create-alert.dto';
 
 @Controller('monitor')
-export class MonitorController {}
+export class MonitorController {
+  constructor(private readonly monitorService: MonitorService) {}
+
+  @Get('/alerts')
+  async getAlerts() {
+    return this.monitorService.getAlerts();
+  }
+
+  @Post('/alerts')
+  async createAlert(@Body() createAlertDto: CreateAlertDto) {
+    return this.monitorService.createAlert(createAlertDto);
+  }
+
+  @Delete('/alerts/:id')
+  @HttpCode(204)
+  async removeAlert(@Param('id') id: string) {
+    return this.monitorService.removeAlert(id);
+  }
+}
