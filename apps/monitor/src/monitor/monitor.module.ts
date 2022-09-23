b5830a856c33ee54241b46327cee7d50dbbc2c20
Chun-Kai Wang <chunkai1312@gmail.com>
2022-09-23 17:52:33 +0800

    feat(monitor): add fugle realtime module

--- apps/monitor/src/monitor/monitor.module.ts
+++ apps/monitor/src/monitor/monitor.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { FugleRealtimeModule } from '@fugle/realtime-nest';
 
-@Module({})
+@Module({
+  imports: [
+    FugleRealtimeModule.registerAsync({
+      useFactory: () => ({
+        apiToken: process.env.FUGLE_REALTIME_API_TOKEN,
+      }),
+    }),
+  ],
+})
 export class MonitorModule {}
