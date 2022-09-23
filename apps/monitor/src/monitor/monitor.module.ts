c2279182d04597987f45a181e856ba8b3557738b
Chun-Kai Wang <chunkai1312@gmail.com>
2022-09-23 18:37:25 +0800

    feat(monitor): add monitor service

--- apps/monitor/src/monitor/monitor.module.ts
+++ apps/monitor/src/monitor/monitor.module.ts
@@ -1,13 +1,21 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { FugleRealtimeModule } from '@fugle/realtime-nest';
+import { Monitor, MonitorSchema } from './monitor.schema';
+import { MonitorRepository } from './monitor.repository';
+import { MonitorService } from './monitor.service';
 
 @Module({
   imports: [
+    MongooseModule.forFeature([
+      { name: Monitor.name, schema: MonitorSchema },
+    ]),
     FugleRealtimeModule.registerAsync({
       useFactory: () => ({
         apiToken: process.env.FUGLE_REALTIME_API_TOKEN,
       }),
     }),
   ],
+  providers: [MonitorRepository, MonitorService],
 })
 export class MonitorModule {}
