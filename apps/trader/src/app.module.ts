ca61c9e2e6374fdcceb03e979795632a3ad9838c
Chun-Kai Wang <chunkai1312@gmail.com>
2022-09-26 15:57:39 +0800

    chore(trader): setup app module

--- apps/trader/src/app.module.ts
+++ apps/trader/src/app.module.ts
@@ -1,4 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { FugleTradeModule } from '@fugle/trade-nest';
+import { LineNotifyModule } from 'nest-line-notify';
 
-@Module({})
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    FugleTradeModule.forRoot({
+      config: {
+        apiUrl: process.env.FUGLE_TRADE_API_URL,
+        certPath: process.env.FUGLE_TRADE_CERT_PATH,
+        apiKey: process.env.FUGLE_TRADE_API_KEY,
+        apiSecret: process.env.FUGLE_TRADE_API_SECRET,
+        aid: process.env.FUGLE_TRADE_AID,
+        password: process.env.FUGLE_TRADE_PASSWORD,
+        certPass: process.env.FUGLE_TRADE_CERT_PASS,
+      },
+    }),
+    LineNotifyModule.forRoot({
+      accessToken: process.env.LINE_NOTIFY_ACCESS_TOKEN,
+    }),
+  ],
+})
 export class AppModule {}
