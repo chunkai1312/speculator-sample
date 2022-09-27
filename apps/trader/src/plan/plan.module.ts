335c97a3986d0d5f90760e10570b915d3f4cc244
Chun-Kai Wang <chunkai1312@gmail.com>
2022-09-28 01:56:47 +0800

    feat(trader): add plan service

--- apps/trader/src/plan/plan.module.ts
+++ apps/trader/src/plan/plan.module.ts
@@ -1,4 +1,21 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { FugleRealtimeModule } from '@fugle/realtime-nest';
+import { Plan, PlanSchema } from './plan.schema';
+import { PlanRepository } from './plan.repository';
+import { PlanService } from './plan.service';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Plan.name, schema: PlanSchema },
+    ]),
+    FugleRealtimeModule.registerAsync({
+      useFactory: () => ({
+        apiToken: process.env.FUGLE_REALTIME_API_TOKEN,
+      }),
+    }),
+  ],
+  providers: [PlanRepository, PlanService],
+})
 export class PlanModule {}
