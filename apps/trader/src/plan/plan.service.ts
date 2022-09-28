c43fcd6d9dafd83680708319299d7cd4040c0276
Chun-Kai Wang <chunkai1312@gmail.com>
2022-09-28 14:23:47 +0800

    feat(trader): execute sip

--- apps/trader/src/plan/plan.service.ts
+++ apps/trader/src/plan/plan.service.ts
@@ -1,11 +1,12 @@
 import { omit } from 'lodash';
 import { DateTime } from 'luxon';
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
 import { HttpClient } from '@fugle/realtime';
 import { FugleTrade, Order } from '@fugle/trade';
 import { InjectHttpClient } from '@fugle/realtime-nest';
 import { InjectFugleTrade } from '@fugle/trade-nest';
+import { InjectLineNotify, LineNotify } from 'nest-line-notify';
 import { PlanRepository } from './plan.repository';
 import { CreatePlanDto } from './dto/create-plan.dto';
 
@@ -14,6 +15,7 @@ export class PlanService {
   constructor(
     @InjectHttpClient() private readonly client: HttpClient,
     @InjectFugleTrade() private readonly fugle: FugleTrade,
+    @InjectLineNotify() private readonly lineNotify: LineNotify,
     private readonly PlanRepository: PlanRepository,
   ) {}
 
@@ -32,30 +34,53 @@ export class PlanService {
 
   @Cron('0 29 13 * * *')
   async execute() {
-    const date = DateTime.local().toISODate();
-    const plans = await this.PlanRepository.getPlansToExecute(date);
+    const dt = DateTime.local();
+    const date = dt.toISODate();
+    const time = dt.toFormat('yyyy/MM/dd HH:mm:ss');
 
-    for (const plan of plans) {
-      const res = await this.client.intraday.quote({ symbolId: plan.symbol, oddLot: true });
-      if (res.data.info.date !== date) continue;
+    try {
+      const plans = await this.PlanRepository.getPlansToExecute(date);
 
-      const price = res.data.quote.order.asks.pop().price;
-      const quantity =  Math.floor(plan.price / price);
+      for (const plan of plans) {
+        const res = await this.client.intraday.quote({ symbolId: plan.symbol, oddLot: true });
+        if (res.data.info.date !== date) continue;
 
-      const order = new Order({
-        stockNo: plan.symbol,
-        buySell: Order.Side.Buy,
-        price,
-        quantity,
-        apCode: Order.ApCode.IntradayOdd,
-        priceFlag: Order.PriceFlag.Limit,
-        bsFlag: Order.BsFlag.ROD,
-        trade: Order.Trade.Cash,
-      });
-      await this.fugle.placeOrder(order);
+        const price = res.data.quote.order.asks.pop().price;
+        const quantity =  Math.floor(plan.price / price);
 
-      const preorder = plan.preorders.find(preorder => preorder.date <= date && preorder.placed === false);
-      await this.PlanRepository.updateExecutedPlan(plan.id, preorder.date);
+        const order = new Order({
+          stockNo: plan.symbol,
+          buySell: Order.Side.Buy,
+          price,
+          quantity,
+          apCode: Order.ApCode.IntradayOdd,
+          priceFlag: Order.PriceFlag.Limit,
+          bsFlag: Order.BsFlag.ROD,
+          trade: Order.Trade.Cash,
+        });
+
+        // 設定推播訊息
+        const message = [
+          '',
+          `<<定期定額>>`,
+          `股票代號: ${plan.symbol}`,
+          `價格: ${price}`,
+          `數量: ${quantity}`,
+          `時間: ${time}`,
+        ].join('\n');
+
+        // 透過 LINE Notify 推播訊息
+        await this.lineNotify.send({ message });
+
+        // 執行下單委託
+        await this.fugle.placeOrder(order);
+
+        // 將預約訂單更新為已執行
+        const preorder = plan.preorders.find(preorder => preorder.date <= date && preorder.placed === false);
+        await this.PlanRepository.updateExecutedPlan(plan.id, preorder.date);
+      }
+    } catch (err) {
+      Logger.error(err.message, err.stack, PlanService.name);
     }
   }
 }
